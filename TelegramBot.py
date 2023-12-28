26b0c48d3ef23a052daf862640b924430a4fd975
Egor Sennov <93314968+SennovE@users.noreply.github.com>
2023-12-28 12:11:26 +0300

    Доработаны декораторы создания обработчиков

--- TelegramBot.py
+++ TelegramBot.py
@@ -1,11 +1,12 @@
-from telegram.ext import ApplicationBuilder, CommandHandler, filters, CallbackQueryHandler
-from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler
 
 class TelegramBot():
     '''
     A class that creates a bot for telegram, with the ability to add handlers and then launch
     '''
     __slots__ = ["token", "online", "app"]
+    handlers = []
+    job_queue = []
 
     def __init__(self, token: str) -> None:
         self.token = token
@@ -19,6 +20,14 @@ class TelegramBot():
             return False
         
         else:
+            self.app.add_handlers(TelegramBot.handlers)
+
+            for job in TelegramBot.job_queue:
+                if job["repeating"]:
+                    self.app.job_queue.run_repeating(callback=job["func"], interval=job["interval"], first=job["first"])
+                else:
+                    self.app.job_queue.run_once(callback=job["func"], when=job["first"])
+
             self.app.run_polling()
             return True
         
@@ -31,39 +40,42 @@ class TelegramBot():
             return False
         
 
-    def AddCommandHandler(self, command: str, filters=None):
+    @staticmethod
+    def AddCommandHandler(command: str, filters=None):
         def decorator(func):
             async def wrapper(update, context, *args, **kwargs):
                 return await func(update, context, *args, **kwargs)
 
             handler = CommandHandler(command=command, callback=wrapper, filters=filters)
-            self.app.add_handler(handler)
+            TelegramBot.handlers.append(handler)
+
             return handler
 
         return decorator
 
 
-    def AddCallbackQueryHandler(self, pattern=None):
+    @staticmethod
+    def AddCallbackQueryHandler(pattern=None):
         def decorator(func):
             async def wrapper(update, context, *args, **kwargs):
                 return await func(update, context, *args, **kwargs)
 
             handler = CallbackQueryHandler(callback=wrapper, pattern=pattern)
-            self.handlers.append(handler)
+            TelegramBot.handlers.append(handler)
+
             return handler
 
         return decorator
     
-    
-    def AddJobQuery(self, repeating=False, first=None, interval=86400.0):
+
+    @staticmethod
+    def AddJobQuery(repeating=False, first=None, interval=86400.0):
         def decorator(func):
             async def wrapper(context, *args, **kwargs):
                 return await func(context, *args, **kwargs)
             
-            if repeating:
-                self.app.job_queue.run_repeating(wrapper, interval=interval, first=first)
-            else:
-                self.app.job_queue.run_once(wrapper, when=first)
+            TelegramBot.job_queue.append({"repeating": repeating, "func": wrapper, "interval": interval, "first": first})
+
             return wrapper
 
         return decorator
